d01d366af1f7cf4cb94f64ce2330008469ef2869
Vishnu Shukla <110892073+vishnushukla09@users.noreply.github.com>
2023-04-13 09:36:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("300x300")
+window.title("simple calculater")
+
+lbl1 = Label(window,text="enter 1st number : ")
+lbl1.place(x=50,y=50)
+
+lbl2 = Label(window,text="enter 2nd number : ")
+lbl2.place(x=50,y=100)
