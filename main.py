6622d3822bd4456159eafbbc77c39da7c29f7083
vishnushukla09 <110892073+vishnushukla09@users.noreply.github.com>
2023-04-13 10:18:42 +0000

    complete basic calculater

--- main.py
+++ main.py
@@ -31,3 +31,39 @@ def add():
 b1 =Button (window, text= "Add",command= add)
 b1.place(x=50, y=200)
 
+def sub():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sum = num1 - num2
+  t3.insert(END,str(sum))
+
+b2 =Button (window, text= "Subtract",command= sub)
+b2.place(x=120, y=200)
+
+def mul():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sum = num1 * num2
+  t3.insert(END,str(sum))
+  
+b3 =Button (window, text= "Multiply",command= mul)
+b3.place(x=220, y=200)
+
+def div():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sum = num1 / num2
+  t3.insert(END,str(sum))
+
+b4 =Button (window, text= "Divide",command= div)
+b4.place(x=320, y=200)
+
+def clear():
+  t1.delete(0,END)
+  t2.delete(0,END)
+  t3.delete(0,END)
+  
+
+
+b5 =Button (window, text= "CLEAR",command= clear)
+b5.place(x=190, y=250)
